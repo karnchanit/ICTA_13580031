67ed9987e37279f9f5e0b9b1b199a98e34e65b40
karn.chanit101@gmail.com <@pe2kjbw>
2017-12-21 23:41:02 +0700

    Project_A1

---
+++ ICTA13580031/ICTA13580031Page.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace ICTA13580031
+{
+    public partial class ICTA13580031Page : ContentPage
+    {
+
+
+        public ICTA13580031Page()
+        {
+            InitializeComponent();
+
+            okButton.Clicked += OkButton_Clicked;
+
+        }
+
+
+        void OkButton_Clicked(object sender, System.EventArgs e)
+        {
+            firstNameLabel.Text = firstNameEntry.Text;
+        }
+    }
+}
